d3dab537ae16d90de34512c2f7a4cb9e4b5a62fd
Kimberly Mageary <kimberlymageary@gmail.com>
2024-10-03 14:33:32 -0700

    Created a new deer class

---
+++ src/Deer.java
@@ -0,0 +1,15 @@
+public class Deer implements Animal {
+
+    @Override
+    public String getAnimalArt() {
+     return
+        "     \\   (             )\n" +
+        "      \\    `--(_   _)--\'\n" +
+        "       \\       Y-Y\n" +
+        "        \\     /@@ \\\n" +
+        "         \\   /     \\\n" +
+        "             `--\'.  \\             ,\n" +
+        "                 |   `.__________/)";
+    }
+
+}
