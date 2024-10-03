f42885e8843952c0eed1e29259803206015d2f9e
Rebecca Riffle <rebeccaeriffle@gmail.com>
2024-10-03 14:45:54 -0700

    Added dog

---
+++ src/Dog.java
@@ -0,0 +1,27 @@
+public class Dog implements Animal{
+    /**
+   * Returns the ASCII art representation of the dog.
+   *
+   * @return A string containing the ASCII art of the dog.
+   */
+  @Override
+  public String getAnimalArt() {
+      return "        \\   .\n" +
+             "         \\  ..^____/\n" +
+             "            `-. ___ )\n" +
+             "              ||   ||\n";
+  }
+
+  /**
+   * Returns the name of the animal ("dog").
+   * This method overrides the toString method to return the name of the dog.
+   *
+   * @return The string "dog" representing the name of the animal.
+   */
+  @Override
+  public String toString() {
+    return "dog";
+  }
+    
+}
+
