546c3561f596aa1f48bb3b03dcdb2740a364103c
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-10-07 11:25:41 -0700

    added a Dog class and modified the SayApp class to offer another option

---
+++ src/Dog.java
@@ -0,0 +1,33 @@
+/**
+ * The Dog class represents a dog and provides its ASCII art representation.
+ * This class implements the Animal interface and overrides the getAnimalArt 
+ * and toString methods.
+ * 
+ * dog by Tony Monroe. See license file for more details.
+ */
+class Dog implements Animal {
+
+    /**
+     * Returns the ASCII art representation of the dog.
+     *
+     * @return A string containing the ASCII art of the dog.
+     */
+    @Override
+    public String getAnimalArt() {
+        return  "        \\   .\n" +
+                "         \\  ..^____/\n" +
+                "            `-. ___ )\n" +
+                "              ||   ||\n";
+    }
+  
+    /**
+     * Returns the name of the animal ("dog").
+     * This method overrides the toString method to return the name of the dog.
+     *
+     * @return The string "dog" representing the name of the animal.
+     */
+    @Override
+    public String toString() {
+      return "dog";
+    }
+  }
